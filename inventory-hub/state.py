0c3457eb6672b539461cbcc3ed48d2872f18b1df
derekp10 <derekp10@gmail.com>
2026-02-01 18:30:24 -0800

    1. state.py (The Memory) Handles logging, the Undo Stack, and global variables.

---
+++ inventory-hub/state.py
@@ -0,0 +1,45 @@
+import logging
+import sys
+import time
+from logging.handlers import RotatingFileHandler
+
+# --- GLOBAL STATE ---
+UNDO_STACK = []
+RECENT_LOGS = []
+
+# --- LOGGING SETUP ---
+logger = logging.getLogger("InventoryHub")
+logger.setLevel(logging.INFO)
+
+# Console Output
+c_handler = logging.StreamHandler(sys.stdout)
+c_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+logger.addHandler(c_handler)
+
+# File Output
+f_handler = RotatingFileHandler('hub.log', maxBytes=1000000, backupCount=5)
+f_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+logger.addHandler(f_handler)
+
+def add_log_entry(msg, category="INFO", color_hex=None):
+    """Adds a log entry to the in-memory list and the system log."""
+    timestamp = time.strftime("%H:%M:%S")
+    
+    # Visual Swatch for the UI log
+    if color_hex:
+        swatch = f'<span style="display:inline-block;width:24px;height:24px;border-radius:50%;background-color:#{color_hex};margin-right:10px;border:2px solid #fff;vertical-align:middle;"></span>'
+        ui_msg = swatch + f'<span style="vertical-align:middle;">{msg}</span>'
+    else:
+        ui_msg = msg
+
+    entry = {"time": timestamp, "msg": ui_msg, "type": category}
+    RECENT_LOGS.insert(0, entry)
+    if len(RECENT_LOGS) > 50: RECENT_LOGS.pop()
+    
+    # Standard Python Logging
+    if category == "ERROR":
+        logger.error(msg)
+    elif category == "WARNING":
+        logger.warning(msg)
+    else:
+        logger.info(msg)
