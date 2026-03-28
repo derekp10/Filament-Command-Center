a66940e3efaf7afaccfc759df170128d6751c612
derekp10 <derekp10@gmail.com>
2026-03-27 20:38:38 -0700

    refactor: remove development test scripts and update archive documentation.

--- test_sqlite.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-db_path = r'\\TRUENAS\App_Data\Spoolman\SPOOLMAN_DIR_DATA\spoolman.db'
-conn = sqlite3.connect(db_path)
-cur = conn.cursor()
-cur.execute("SELECT sql FROM sqlite_master WHERE name='spool'")
-print(cur.fetchone()[0])
-conn.close()
--- test_type.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-import json
-db_path = r'\\TRUENAS\App_Data\Spoolman\SPOOLMAN_DIR_DATA\spoolman.db'
-conn = sqlite3.connect(db_path)
-cur = conn.cursor()
-cur.execute("SELECT filament_id, key, value FROM filament_field WHERE key = 'filament_attributes' LIMIT 10")
-for r in cur.fetchall():
-    print(r[0], repr(r[1]), repr(r[2]))
-conn.close()
